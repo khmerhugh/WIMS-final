930ac7ae9d4ffe6dddff741660b4962a8e78ca48
Hugh Johnson <hugh@boilermaker.net>
2024-11-03 10:44:32 -0500

    big change

--- server/urls.py
+++ server/urls.py
@@ -1,22 +1,16 @@
-"""
-URL configuration for server project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, re_path, include
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+#from .views import get_files, create_file, file_detail
+from .views import *
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', homepage),
+    re_path('login', login),
+    re_path('signup', signup),
+    re_path('test_token', test_token),
+    path('files', files),
+    path('files/<int:pk>', file_detail, name='file_detail'),
+    path('files/create/', create_file, name='create_file'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
